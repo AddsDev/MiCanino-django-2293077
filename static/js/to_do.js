b273009779ff79fafed8c75d5f9cb7fe7fe638a0
Adrian Ruiz <adrianruiz.981031@gmail.com>
2021-10-28 16:58:27 -0500

    add eventlisterner click (fix)

--- static/js/to_do.js
+++ static/js/to_do.js
@@ -5,9 +5,8 @@ const list_fin = document.getElementById("finalizadas")
 
 agregar.onclick = ()=>{
     var valor = tarea.value
-
     list_pen.appendChild(getCard(valor, true))
-
+    addListeners()
 }
 
 function getCard(valor, opcion) {
@@ -25,17 +24,50 @@ function getCard(valor, opcion) {
     tarea.classList = "tarea"
     agregar.classList ="fin"
     eliminar.classList ="eliminar"
+    card.setAttribute('data',"0")
 
     card.appendChild(tarea)
     if(opcion){
         card.appendChild(agregar)
         card.appendChild(eliminar)
-        agregar.onclick= ()=>{
-            list_fin.appendChild(getCard(valor, false))
-        }
     }
     
-
     
     return card
+}
+
+const addListeners = () => {
+
+    
+    list_pen.childNodes.forEach((card) => {
+        if(card.childNodes.length == 3 && card.getAttribute('data') == "0"){
+            card.childNodes.forEach((element) => {
+                element.addEventListener('click', cardAction, false)
+                card.setAttribute('data', "1") 
+            })
+        }   
+    })
+
+    function cardAction(e){
+        var div = e.target.parentNode
+        switch(e.target.className){
+            case "fin":
+                div.childNodes.forEach((element)=>{
+                    if(element.type == "submit")
+                        element.setAttribute('hidden','hidden')
+                })
+                list_fin.appendChild(div)
+                //list_pen.removeChild(e.target.parentNode)
+                break;
+            case "eliminar":
+                list_pen.removeChild(div)
+                break;
+        }
+
+        
+    }
+
+    // agregar.onclick= ()=>{
+    //     list_fin.appendChild(getCard(valor, false))
+    // }
 }
